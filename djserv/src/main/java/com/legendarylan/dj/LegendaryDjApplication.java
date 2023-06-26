9a06f31a4a79a57d17281692bad2a9c88c1afef4
Aaron Saraco <sirlemming@gmail.com>
2023-06-26 11:22:59 -0400

    Initial commit. -AMS

---
+++ djserv/src/main/java/com/legendarylan/dj/LegendaryDjApplication.java
@@ -0,0 +1,13 @@
+package com.legendarylan.dj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegendaryDjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegendaryDjApplication.class, args);
+	}
+
+}
---
+++ djserv/src/test/java/com/legendarylan/dj/LegendaryDjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legendarylan.dj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegendaryDjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
