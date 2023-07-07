bec76e0a9cf1e71abe67c1a2ca46bd00428b3e1c
Aaron Saraco <sirlemming@gmail.com>
2023-07-07 12:47:19 -0400

    AMS 7/7 trying again

--- djserv/src/main/java/com/legendarylan/dj/LegendaryDjApplication.java
+++ djserv/src/main/java/com/legendarylan/dj/LegendaryDjApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LegendaryDjApplication {
+	
+	//AMS Test comment, will it get committed? Pull to find out!
 
 	public static void main(String[] args) {
 		SpringApplication.run(LegendaryDjApplication.class, args);
