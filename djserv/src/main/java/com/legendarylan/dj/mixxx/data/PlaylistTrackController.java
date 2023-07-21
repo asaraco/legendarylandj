11f5cb62829effdb20aed72ce383004d7172cfb8
Aaron Saraco <sirlemming@gmail.com>
2023-07-21 15:25:48 -0400

    7/21/2023 AMS - corrected playlist track insert method (params were switched)

--- djserv/src/main/java/com/legendarylan/dj/mixxx/data/PlaylistTrackController.java
+++ djserv/src/main/java/com/legendarylan/dj/mixxx/data/PlaylistTrackController.java
@@ -18,7 +18,7 @@ class PlaylistTrackController {
 	
 	private final PlaylistTrackRepository repo;
 	private final TrackRepository library;
-	private final int autoDjPlaylistId = 1;
+	private final int playlistId = 1;	// Auto DJ playlist
 
 	PlaylistTrackController(PlaylistTrackRepository repo, TrackRepository library) {
 		this.repo = repo;
@@ -29,9 +29,10 @@ class PlaylistTrackController {
 	@ResponseBody
 	ResponseEntity<?> requestSong(@Param("id") String id) {
 		int latest = 3;	//TODO: Calculate max position in playlist
+		int position = latest + 1;
 		//Optional<Track> song = library.findById(Long.parseLong(sId));
-		repo.save(new PlaylistTrack(autoDjPlaylistId, latest, Integer.parseInt(id)));
-		return ResponseEntity.ok("Success");
+		repo.save(new PlaylistTrack(playlistId, Integer.parseInt(id), position));
+		return ResponseEntity.ok("Success: track " + id + " added to playlist " + playlistId + " at position " + position);
 	}
 
 }
