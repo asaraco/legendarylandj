86ef222856793f9263a63e9b8951dd0c7fdb61f5
Aaron Saraco <sirlemming@gmail.com>
2023-08-02 19:46:18 -0400

    8/2/2023 AMS - added Cross-Origin annotation to endpoints

--- djserv/src/main/java/com/legendarylan/dj/mixxx/data/PlaylistRepository.java
+++ djserv/src/main/java/com/legendarylan/dj/mixxx/data/PlaylistRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(collectionResourceRel="playlists", path="playlists")
+@CrossOrigin("http://localhost:4200")
 public interface PlaylistRepository extends CrudRepository<Playlist, Long> {
 	List<Playlist> findByName(@Param("name") String name);
 	Playlist findTopByOrderByIdDesc();	// determine maximum playlist ID #
--- djserv/src/main/java/com/legendarylan/dj/mixxx/data/PlaylistTrackRepository.java
+++ djserv/src/main/java/com/legendarylan/dj/mixxx/data/PlaylistTrackRepository.java
@@ -2,8 +2,10 @@ package com.legendarylan.dj.mixxx.data;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(collectionResourceRel="playlistTracks", path="playlistTracks")
+@CrossOrigin("http://localhost:4200")
 public interface PlaylistTrackRepository extends CrudRepository<PlaylistTrack, Long> {
 	PlaylistTrack findFirstByPlaylistIdOrderByPositionDesc(int playlistId);	// determines highest "position" value in specified playlist
 }
