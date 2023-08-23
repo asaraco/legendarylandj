2a8c7115a89b7ed54399d8f1a5e4905e4293fc83
Aaron Saraco <sirlemming@gmail.com>
2023-08-22 20:41:06 -0400

    Added find by artist starting with letter, and enabled cross-origin

--- djserv/src/main/java/com/legendarylan/dj/mixxx/data/TrackRepository.java
+++ djserv/src/main/java/com/legendarylan/dj/mixxx/data/TrackRepository.java
@@ -5,9 +5,12 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(collectionResourceRel="tracks", path="tracks")
+@CrossOrigin("http://localhost:4200")
 public interface TrackRepository extends CrudRepository<Track, Long> {
 	Track findById(@Param("id") int id);
 	List<Track> findByArtist(@Param("artist") String artist);
+	List<Track> findByArtistStartingWith(@Param("ch") String character);
 }
