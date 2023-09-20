9459547f5489e828efba30c56a00da1b0e6b80b9
Aaron Saraco <sirlemming@gmail.com>
2023-09-19 21:27:53 -0400

    1st version of file upload service

--- djserv/src/main/java/com/legendarylan/dj/Controllers.java
+++ djserv/src/main/java/com/legendarylan/dj/Controllers.java
@@ -1,8 +1,15 @@
 package com.legendarylan.dj;
 
+import java.io.File;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class Controllers {
@@ -11,5 +18,17 @@ public class Controllers {
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
       return String.format("Hello %s!", name);
     }
+	
+	@CrossOrigin({"http://localhost:4200", "http://"+Const.LOCALHOST_IP+":4200"})
+	@PostMapping("/upload")
+	public ResponseEntity<?> handleFileUpload( @RequestParam("song") MultipartFile file ) {
+		String fileName = file.getOriginalFilename();
+		try {
+			file.transferTo(new File("C:\\Users\\Public\\Music\\LANtrax\\" + fileName));
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+		return ResponseEntity.ok("File uploaded successfully - " + fileName);
+	}
 
 }
