cf1c27bb183bf87139a214d212ddcd8b4cf7cc63
Aaron Saraco <sirlemming@gmail.com>
2023-10-19 22:00:01 -0400

    Styling & mobile layout changes

--- djweb/src/app/app.component.ts
+++ djweb/src/app/app.component.ts
@@ -13,7 +13,9 @@ export class AppComponent implements OnInit {
   title = 'djweb';
   mostRecentPlaylist: Playlist = new Playlist();
   autoDjPlaylist: Playlist = new Playlist();
-  showPlayHistory: boolean = false;
+  showMenu: boolean = false;
+  showQueue: boolean = false;  
+  showNew: boolean = false;
   //selectedCrate: number = 0;
   //alphabet: string[] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
@@ -49,26 +51,26 @@ export class AppComponent implements OnInit {
     }
   }
 
-  togglePlayHistory(): void {
-    this.showPlayHistory = !this.showPlayHistory;
-  }
-  /*
-  artistsBy(ch: string) {
-    this.router.navigate(['artistsBy/', ch]);
+  toggleHamburgerMenu(): void {
+    this.showMenu = !this.showMenu;
   }
 
-  browseAll() {
-    this.router.navigate(['browseAll']);
+  toggleQueue(): void {
+    this.showQueue = !this.showQueue;
+    this.showNew = false;
+    this.showMenu = false;
+  }  
+
+  toggleNewArrivals(): void {
+    this.showNew = !this.showNew;
+    this.showQueue = false;
+    this.showMenu = false;
   }
 
-  selectCrate(id: number) {
-    if (this.selectedCrate!=id) {
-      this.selectedCrate = id;
-    } else {
-      this.selectedCrate = 0;
-    }
-    
-    console.log(`selectedCrate = ${this.selectedCrate}`);
+  resetView(): void {
+    this.showNew = false;
+    this.showQueue = false;
+    this.showMenu = false;
   }
-  */
+
 }
